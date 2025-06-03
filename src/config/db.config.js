f4ed1e956a0a42dbc62ee6a40926f72a61762c60
Aneesh Kolar <kolaraneesh2003@gmail.com>
2025-06-03 21:12:38 +0530

    Changed the db.config.js file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,11 +3,11 @@ dotenv.config();
 
 const config = {
   development: {
-    username: 'root',
-    password: 'aneesh123',
-    database: 'airbnb_dev',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USER, // 'root'
+    password: process.env.DB_PASSWORD, // 'aneesh123'
+    database: process.env.DB_NAME, // 'airbnb_dev'
+    host: process.env.DB_HOST, // '127.0.0.1'
+    dialect: 'mysql', // 'mysql'
   }
 }
 
